784da2793a90f22c70e27127cc98fd2179c591fd
sans <clafernalo@gmail.com>
2023-01-16 17:55:11 +0800

    a special chinese new year gift (work) for james

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -16,53 +16,81 @@ import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/system";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js'
+
 
 const drawerWidth = 240;
 
 function AppLayout({ children }) {
+    const [time, setTime] = React.useState(new Date());
+
+    React.useEffect(()=>{
+        import('bootstrap/dist/js/bootstrap.min.js');
+
+        const timeUpdater = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timeUpdater);
+      },[]);
+
     const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const [active, setActive] = React.useState(0); //which menu is currently selected
 
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
 
+    const menuList = [
+        {id: 0, name: "Overview", icon: "", url: ""},
+        {id: 1, name: "Inventory", icon: "shipping.png", url: ""},
+        {id: 2, name: "Data", icon:"database.png", url: ""}
+    ];
+
+
     const drawer = (
-        <div>
-            <Toolbar />
-            <Divider />
-            <List>
-                {["Inbox", "Starred", "Send email", "Drafts"].map(
-                    (text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? (
-                                        <InboxIcon />
-                                    ) : (
-                                        <MailIcon />
-                                    )}
-                                </ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItemButton>
-                        </ListItem>
-                    )
-                )}
-            </List>
-            <Divider />
-            <List>
-                {["All mail", "Trash", "Spam"].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+        <div 
+        className="border border-0 side-nav d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" 
+        >
+            <div className="d-flex align-items-start justify-content-center nav-logo">
+            <img src="singtel-logo.png" width="125px"/>
+            </div>
+            <ul className="nav nav-pills flex-column mb-auto">
+            {menuList.map((menu, index) => (
+                <li className="nav-item mb-2" key={index}>
+                <a 
+                    href={menu.url} 
+                    className= {`d-flex align-items-center nav-link ${menu.id==active ? 'sidebar-active' : ''}`} 
+                    aria-current="page"
+                    style={{
+                        boxShadow: "0px 3px 5px 1px rgba(0,0,0,0.2)"
+                    }} 
+                >
+                    <div className="me-3 ms-4 mt-1 mb-1">
+                    <img className="menu-logo" src={menu.icon} style={{width:"20px"}}/>
+                    </div>
+                    <div className="text-white fw-bold">
+                    {menu.name}
+                    </div>
+                </a>
+                </li>
+            ))}
+            </ul>
+            
         </div>
     );
+    
+    const header_theme = createTheme({
+        typography: {
+            font: 'Kamerik',
+        }
+    });
+
+    var date = `${new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(time)}, ${time.getDate()} ${new Intl.DateTimeFormat("en-US", { month: "long" }).format(time)} ${time.getFullYear()}`
+    var clock = `${time.getHours()%24}:${time.getMinutes()}`
 
     return (
         <Box sx={{ display: "flex" }}>
@@ -74,7 +102,12 @@ function AppLayout({ children }) {
                     ml: { sm: `${drawerWidth}px` },
                 }}
             >
-                <Toolbar>
+                <Toolbar
+                    sx={{
+                        backgroundColor: "#32353E",
+                        boxShadow: "0px 0px 0px"
+                    }}
+                >
                     <IconButton
                         color="inherit"
                         aria-label="open drawer"
@@ -84,9 +117,34 @@ function AppLayout({ children }) {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" noWrap component="div">
+                    <div 
+                        className="header container text-center d-none d-sm-block"
+                    >
+                        <div className="row align-items-start">
+                            <div 
+                                className="col justify-content-start align-items-center"
+                                style={{
+                                    height:"100%",
+                                }}
+                            >
+                                    {menuList[active].name}
 
-                    </Typography>
+                            </div>
+
+                            <div 
+                                className="col justify-content-center d-flex flex-column time"
+                            >
+                                <div className="d-flex justify-content-center">{date}</div>
+                                <div className="d-flex justify-content-center">{clock}</div>
+                            </div>
+
+                            <div 
+                                className="col justify-content-end d-flex">
+                                orz joshua jamez
+                            </div>
+                        </div>
+                    </div>
+                    
                 </Toolbar>
             </AppBar>
             <Box
@@ -128,6 +186,7 @@ function AppLayout({ children }) {
             <Box
                 component="main"
                 sx={{
+                    backgroundColor: "#32353E",
                     flexGrow: 1,
                     p: 3,
                     width: { sm: `calc(100% - ${drawerWidth}px)` },
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
